77c3cb4c699a4e45bf9bca312351d8e68136d87f
tarripewe <tarripewe01@gmail.com>
2022-11-17 21:12:19 +0700

    Controller: Preventing Duplicate User

--- controllers/user.js
+++ controllers/user.js
@@ -3,9 +3,14 @@ const UserModel = require("../models/user");
 exports.create = async (req, res) => {
   const { name, email, password } = req.body;
 
+  const oldUser = await UserModel.findOne({ email });
+
+  if (oldUser)
+    return res.status(401).json({ error: "This Email is already in use" });
+
   const newUser = new UserModel({ name, email, password });
 
   await newUser.save();
 
-  res.json({ user: newUser });
+  res.status(201).json({ user: newUser });
 };
