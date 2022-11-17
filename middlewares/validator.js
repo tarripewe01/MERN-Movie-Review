160bd47c9c1a37714b1903dca63f08c5954a18fe
tarripewe <tarripewe01@gmail.com>
2022-11-17 21:32:49 +0700

    Middlewares: Express Validator

---
+++ middlewares/validator.js
@@ -0,0 +1,22 @@
+const { check, validationResult } = require("express-validator");
+
+exports.userValidator = [
+  check("name").trim().not().isEmpty().withMessage("Name is missing !"),
+  check("email").normalizeEmail().isEmail().withMessage("Email is invalid !"),
+  check("password")
+    .trim()
+    .not()
+    .isEmpty()
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long !"),
+];
+
+exports.validate = (req, res, next) => {
+  const errors = validationResult(req).array();
+  if (errors.length) {
+    return res.status(400).json({ error: errors[0].msg });
+  }
+  console.log(errors);
+
+  next();
+};
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const { check } = require("express-validator");
 const router = express.Router();
 
 const { create } = require("../controllers/user");
+const { userValidator, validate } = require("../middlewares/validator");
 
-
-router.post("/create", create)
+router.post("/create", userValidator, validate, create);
 
 module.exports = router;
