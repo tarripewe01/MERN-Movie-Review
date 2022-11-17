249081b68fcd7385e50bbcbdd3127602870bb14d
tarripewe <tarripewe01@gmail.com>
2022-11-17 20:58:05 +0700

    Controller: Hash Password

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -17,5 +18,12 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+}); // 1
 
 module.exports = mongoose.model("User", userSchema);
