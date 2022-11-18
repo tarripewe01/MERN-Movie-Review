2a454c653a918bc6f78da419b7e6d170dc5dc206
tarripewe <tarripewe01@gmail.com>
2022-11-18 19:16:52 +0700

    Models: Reset Password

---
+++ models/passwordResetToken.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const passwordResetTokenSchema = new mongoose.Schema({
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  createAt: {
+    type: Date,
+    expires: 3600,
+    default: Date.now,
+  },
+});
+
+passwordResetTokenSchema.pre("save", async function (next) {
+  if (this.isModified("token")) {
+    this.token = await bcrypt.hash(this.token, 10);
+  }
+  next();
+});
+
+passwordResetTokenSchema.methods.compaireToken = async function (token) {
+  return await bcrypt.compare(token, this.token);
+};
+
+module.exports = mongoose.model(
+  "PasswordResetToken",
+  passwordResetTokenSchema
+);
