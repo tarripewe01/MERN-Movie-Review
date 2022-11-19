8434765ccd1f96d384cf42876d1f5118e51d868f
tarripewe <tarripewe01@gmail.com>
2022-11-19 23:03:19 +0700

    Uploading Video Trailer to the Cloudinary

--- controllers/movie.js
+++ controllers/movie.js
@@ -1,5 +1,26 @@
-const MovieModel = require('../models/movie');
+const MovieModel = require("../models/movie");
+const { sendError } = require("../utils/helper");
+
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+  secure: true,
+});
 
 exports.uploadTrailer = async (req, res) => {
+  const { file } = req;
 
-}
+  if (!file) return sendError(res, "Video file is missing");
+
+  const { secure_url: url, public_id } = await cloudinary.uploader.upload(
+    file.path,
+    {
+      resource_type: "video",
+    }
+  );
+
+  res.status(201).json({ url, public_id });
+};
